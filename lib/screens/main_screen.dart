9f761e2e161a22670683c7af9992911271a9c509
aauvray <auvrayarma@gmail.com>
2025-11-09 16:56:39 +0100

    correction

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../utils/app_sections.dart';
 import 'scan_screen/scan_screen.dart';
-import 'profile_screen.dart';
+import 'profile_screen/profile_screen.dart';
 import 'programs_screen.dart';
 
 class MainScreen extends StatefulWidget {
